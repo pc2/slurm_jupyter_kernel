16188adb633b09dc4b82de0217bfd6be65ad6450
Marcel-Brian Wilkowsky <marcel.wilkowsky@uni-paderborn.de>
2022-10-05 09:29:45 +0200

    send extra environment variables into slurm session

--- slurm_jupyter_kernel/start_kernel.py
+++ slurm_jupyter_kernel/start_kernel.py
@@ -5,6 +5,7 @@ import json;
 import pexpect;
 import logging;
 import os;
+import ast;
 import subprocess;
 
 logging.basicConfig(level=logging.DEBUG);
@@ -12,7 +13,7 @@ logging.basicConfig(level=logging.DEBUG);
 # handle kernel as an object
 class remoteslurmkernel:
 
-    def __init__ (self, kernel_cmd, connection_file, slurm_parameter, loginnode, proxyjump, srun_cmd):
+    def __init__ (self, kernel_cmd, connection_file, slurm_parameter, loginnode, proxyjump, srun_cmd, environment):
         
         self.slurm_parameter = slurm_parameter;
         self.kernelcmd = kernel_cmd;
@@ -24,11 +25,14 @@ class remoteslurmkernel:
         self.srun_cmd = 'srun';
         if srun_cmd:
             self.srun_cmd = srun_cmd;
+        self.environment = {};
+        if environment:
+            # make a dict of type string to an actual dictonary
+            self.environment = ast.literal_eval(environment);
 
         self.start_slurm_kernel();
 
     def start_slurm_kernel (self):
-
         cmd_args = [];
         default_slurm_job_name = 'jupyter_slurm_kernel';
 
@@ -55,6 +59,11 @@ class remoteslurmkernel:
         exec_node = self.slurm_session.match.groups()[0];
         self.exec_node = exec_node.decode('utf-8');
         logging.debug(f'Slurm execution node: {self.exec_node}');
+
+        if len(self.environment) >= 1:
+            for key, val in self.environment.items():
+                logging.debug(f"Send to session: export {key}={val}")
+                self.slurm_session.sendline(f"export {key}={val}");
        
         if not self.slurm_session == None:
             self.kernel_connection_info = json.dumps(self.connection_file);
@@ -109,11 +118,12 @@ def slurm_jupyter_kernel ():
     parser.add_argument('--connection-file');
     parser.add_argument('--loginnode');
     parser.add_argument('--proxyjump');
+    parser.add_argument('--environment');
     parser.add_argument('--srun-cmd');
     parser.add_argument('--slurm-parameter', help="Slurm job parameter")
     parser.add_argument('--kernel-cmd', required=True, help='command to run jupyter kernel');
 
     args = parser.parse_args();
 
-    kernel = remoteslurmkernel(args.kernel_cmd, args.connection_file, args.slurm_parameter, args.loginnode, args.proxyjump, args.srun_cmd);
+    kernel = remoteslurmkernel(args.kernel_cmd, args.connection_file, args.slurm_parameter, args.loginnode, args.proxyjump, args.srun_cmd, args.environment);
     kernel.kernel_state();
