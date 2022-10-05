c0a9e1becea911637a6325bfebfd9d56c4ebf694
Marcel-Brian Wilkowsky <marcel.wilkowsky@uni-paderborn.de>
2022-10-05 15:32:18 +0200

    prepare python package

--- setup.py
+++ setup.py
@@ -8,21 +8,21 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setup(
     name='slurm_jupyter_kernel',
-    version='1.3',
-    description='Starting a jupyter kernel using srun',
+    version='1.8',
+    description='Manage and start jupyter slurm kernels',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/mawigh/slurm_jupyter_kernel",
+    url="https://github.com/pc2/slurm_jupyter_kernel",
     project_urls={
-        "Bug Tracker": "https://github.com/mawigh/slurm_jupyter_kernel/issues",
+        "Bug Tracker": "https://github.com/pc2/slurm_jupyter_kernel/issues",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
     ],
     author='Marcel-Brian Wilkowsky',
-    author_email='marcel.wilkowsky@hotmail.de',
+    author_email='marcel.wilkowsky@uni-paderborn.de',
     packages=['slurm_jupyter_kernel'],
     scripts=['bin/slurmkernel'],
-    install_requires=['pexpect', 'pycryptodome', 'jupyter_client', 'IPython']
+    install_requires=['pexpect', 'jupyter_client', 'IPython']
 );
