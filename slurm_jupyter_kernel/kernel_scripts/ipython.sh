b651a93db891f6f82188fe4a72e5d62f799279d5
Marcel-Brian Wilkowsky <marcel.wilkowsky@uni-paderborn.de>
2022-12-20 11:37:13 +0100

    input full path for py venv

--- slurm_jupyter_kernel/kernel_scripts/ipython.sh
+++ slurm_jupyter_kernel/kernel_scripts/ipython.sh
@@ -1,17 +1,17 @@
 # input variables
 # the numbers after INPUT_* will be used as variables inside the script: $1, $2, ...
-INPUT_1=Load required software;module load lang Python
-INPUT_2=Location to install;$HOME
+INPUT_1=Command to load required software;module load lang Python
+INPUT_2=Path to Python virtual env;$HOME/ipython_venv
 
 # commands to execute on remote host
 $1
-python3 -m venv $2/ipython_venv
-source $2/ipython_venv/bin/activate
+python3 -m venv $2
+source $2/bin/activate
 python3 -m pip install ipython ipykernel
 deactivate
-echo -e '#!/bin/bash\n$1\nsource $2/ipython_venv/bin/activate\n"$@"' > $2/ipython_venv/ipy_wrapper.sh
-chmod +x $2/ipython_venv/ipy_wrapper.sh
+echo -e '#!/bin/bash\n$1\nsource $2/bin/activate\n"$@"' > $2/ipy_wrapper.sh
+chmod +x $2/ipy_wrapper.sh
 
 # kernel settings
 LANGUAGE=python
-ARGV=$2/ipython_venv/ipy_wrapper.sh ipython kernel -f {connection_file}
+ARGV=$2/ipy_wrapper.sh ipython kernel -f {connection_file}
